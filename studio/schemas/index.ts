aca682cf5b08297abfa2aade28f599be9ce87b85
McTechie <mc.soans02@gmail.com>
2022-10-22 22:43:34 +0530

    fix: deployment error due to sanity

--- studio/schemas/index.ts
+++
@@ -1,7 +0,0 @@
-import post from './post'
-import comment from './comment'
-import author from './author'
-import category from './category'
-import blockContent from './blockContent'
-
-export const schemaTypes = [post, comment, author, category, blockContent]
